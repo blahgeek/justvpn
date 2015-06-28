c001912286ebcc28b809cfee814f5dd0bf9794fb
BlahGeek <i@BlahGeek.com>
2015-06-29 01:03:07 +0800

    udp: support MTU in config, TTL for remote_addr

--- wire/udp.go
+++ wire/udp.go
@@ -2,7 +2,7 @@
 * @Author: BlahGeek
 * @Date:   2015-06-24
 * @Last Modified by:   BlahGeek
-* @Last Modified time: 2015-06-28
+* @Last Modified time: 2015-06-29
  */
 
 package wire
@@ -11,19 +11,30 @@ import "net"
 import "fmt"
 import "log"
 
+const UDP_REMOTE_ADDR_TTL = 128
+const UDP_DEFAULT_MTU = 1450
+
 type UDPTransport struct {
-	udp         *net.UDPConn
-	remote_addr *net.UDPAddr
-	is_server   bool
+	udp             *net.UDPConn
+	remote_addr     *net.UDPAddr
+	remote_addr_ttl int
+	is_server       bool
+	mtu             int
 }
 
 func (trans *UDPTransport) MTU() int {
-	return 1460 // TODO
+	return trans.mtu
 }
 
 func (trans *UDPTransport) Open(is_server bool, options map[string]interface{}) error {
 	var server_addr, client_addr *net.UDPAddr
 	var err error
+
+	if field := options["mtu"]; field == nil {
+		trans.mtu = UDP_DEFAULT_MTU
+	} else {
+		trans.mtu = field.(int)
+	}
 
 	trans.is_server = is_server
 
@@ -72,10 +83,14 @@ func (trans *UDPTransport) Read(buf []byte) (int, error) {
 		return 0, fmt.Errorf("UDP Transport not available")
 	}
 	rdlen, addr, err := trans.udp.ReadFromUDP(buf)
-	if trans.is_server && err == nil && trans.remote_addr == nil {
-		log.Printf("UDP Server: read from %v", addr)
+	if trans.is_server && err == nil &&
+		(trans.remote_addr == nil || trans.remote_addr_ttl <= 0) {
+		log.Printf("UDP Server update remote address: %v", addr)
 		trans.remote_addr = addr
+		trans.remote_addr_ttl = UDP_REMOTE_ADDR_TTL
 	}
+	trans.remote_addr_ttl -= 1
+
 	return rdlen, err
 }
 
